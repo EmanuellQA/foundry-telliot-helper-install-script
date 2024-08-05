b7078b04dac49e1346f01f021dd50a8a512bb015
EmanuellQA <emanuel@liquidloans.io>
2024-08-04 21:24:55 -0300

    added folder check

--- install.sh
+++ install.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-# Update package list and install necessary system packages
-echo "Creating folder to clone repo..."
-mkdir foundry-telliot-helper
-
+# Check if the directory already exists
 if [ -d "~/foundry-telliot-helper" ]; then
-  echo "Moving to foundry-telliot-helper folder..."
-  cd ~/foundry-telliot-helper
-else
-  echo "Failed to create folder or does not exist."
+  echo "The folder 'foundry-telliot-helper' already exists. Please remove or rename it."
+  exit 1
 fi
+
+# Create the directory if it doesn't exist
+echo "Creating folder to clone repo..."
+mkdir ~/foundry-telliot-helper
+
+# Move to the new directory
+echo "Moving to foundry-telliot-helper folder..."
+cd ~/foundry-telliot-helper
 
 echo "Updating package list..."
 sudo apt update
 
 echo "Cloning repo..."
 git clone git@github.com:EmanuellQA/foundry-telliot-helper.git
+
+if [ $? -eq 0 ]; then
+  echo "Repository cloned successfully."
+else
+  echo "Failed to clone repository."
+  exit 1
+fi
 
 echo "Installing Python 3.10 and venv..."
 sudo apt install -y python3.10 python3.10-venv python3-pip curl
